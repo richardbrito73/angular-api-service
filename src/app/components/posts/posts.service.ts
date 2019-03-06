f31c630fa104622568a691af4b8db9caf49c38e0
Richard Brito <richardbrito@yahoo.com>
2019-03-05 21:21:39 -0400

    Update posts.service.ts

--- src/app/components/posts/posts.service.ts
+++ src/app/components/posts/posts.service.ts
@@ -9,17 +9,24 @@ export class PostsService {
   public url: string;
 
   constructor(public _http: HttpClient) {
+    // Main API url
     this.url = 'https://jsonplaceholder.typicode.com';
   }
-
+  /**
+   * Fetch all posts list
+   */
   fetchPosts(): Observable<any> {
     return this._http.get(this.url + '/posts');
   }
-
+  /**
+   * Get a single post by its ID
+   */
   getPost(id: Number): Observable<any> {
     return this._http.get(this.url + '/posts/' + id);
   }
-
+  /**
+   * Get all comments from a single post
+   */
   getPostComments(id: Number): Observable<any> {
     return this._http.get(this.url + '/posts/' + id + '/comments');
   }
